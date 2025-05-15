19bfaeba5546b3cd5e1a2eb85166e9cf5edb1b92
Kirubel Tesfazion <ktesazi@uoguelph.ca>
2025-05-15 04:17:34 -0400

    began project

---
+++ spotleRemake/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import logo from './assets/Spotify_logo.png';
+
+function App() {
+  return (
+    <>
+    <div className='content'>
+      <div className='center'>
+        <div className='img-div'>
+          <img src={logo}></img>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
